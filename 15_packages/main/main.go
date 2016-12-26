9b4a80e4e23d6d368d3d8c13ced5de4f98a25df5
Nirmal Vatsyayan <nirmal.vatsyayan1990@gmail.com>
2016-12-26 13:38:06 +0530

    helpers

--- 15_packages/main/main.go
+++ 15_packages/main/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
+	"github.com/NirmalVatsyayan/GoPractice/15_packages/helpers"
 )
 
 func main() {
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	helpers.Help("nirmal")
 
 }
