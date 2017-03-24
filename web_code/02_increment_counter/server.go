f0e970ef2ebf8df1952e8d12c98a170cb83a2821
Nirmal Vatsyayan <nirmal.vatsyayan1990@gmail.com>
2017-03-24 14:18:37 +0530

    web dev start

---
+++ web_code/02_increment_counter/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+var counter int
+var mutex = &sync.Mutex{}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	http.HandleFunc("/increment", incrementCounter)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	//http://localhost:8080/increment
+
+}
---
+++ web_code/03_serving_static_file/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
+		fmt.Println(r.URL.Path[1:])
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	//http://localhost:8080/index.html
+
+}
